1e33f36a13237626aa971ab92c7715f35d15494a
Augusto Neves <augusto.agt1995@gmail.com>
2024-04-09 18:56:05 -0300

    feat: creating Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,25 @@
+import { colors } from '@/styles/colors';
+import { ReactNode } from 'react';
+import { TextInput, View, TextInputProps } from 'react-native';
+
+function Input({ children }: { children: ReactNode }) {
+  return (
+    <View className="w-full h-14 flex-row items-center p-3 border border-green-400 rounded-lg my-3">
+      {children}
+    </View>
+  );
+}
+
+function Field({ ...rest }: TextInputProps) {
+  return (
+    <TextInput
+      {...rest}
+      placeholderTextColor={colors.gray[200]}
+      className="flex-1 text-white text-base font-regular pb-2 pl-2"
+    />
+  );
+}
+
+Input.Field = Field;
+
+export { Input };
