c36c287b25d959656ab7adb7b8f5271de0b2e6c9
Augusto Neves <augusto.agt1995@gmail.com>
2024-04-14 16:14:27 -0300

    refactor: refactoring  app layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,4 @@
 import { Slot } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import {
   useFonts,
   Roboto_400Regular,
@@ -7,7 +6,7 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
 import { Loading } from '@/components/Loading/Loading';
-import { SafeAreaView } from 'react-native';
+import { View } from 'react-native';
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
@@ -18,10 +17,9 @@ export default function Layout() {
 
   return (
     <>
-      <StatusBar translucent style="light" />
-      <SafeAreaView className="flex-1 bg-green-500">
+      <View className="flex-1 bg-green-500">
         {fontsLoaded ? <Slot /> : <Loading />}
-      </SafeAreaView>
+      </View>
     </>
   );
 }
