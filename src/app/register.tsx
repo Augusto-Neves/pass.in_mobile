a100faac7de86675bdc790ed1f1ea2d638c757c9
Augusto Neves <augusto.agt1995@gmail.com>
2024-05-08 11:25:15 -0300

    feat: adding event register feature

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Input } from '@/components/Input/Input';
 import { View, Image, Alert, StatusBar } from 'react-native';
 import { FontAwesome6, MaterialIcons } from '@expo/vector-icons';
@@ -5,22 +6,62 @@ import { colors } from '@/styles/colors';
 import { Button } from '@/components/Button/Button';
 import { Link, router } from 'expo-router';
 import { useState } from 'react';
+import { api } from '@/server/api';
+import { useBadgeStore } from '@/store/badge-store';
+
+const EVENT_ID = process.env.EXPO_PUBLIC_EVENT_ID;
 
 export default function Register() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const { saveBadgeData } = useBadgeStore();
 
-  function handleRegister() {
-    if (!name.trim() || !email.trim()) {
-      return Alert.alert('Inscrição', 'Preencha todos os campos.');
+  async function handleRegister() {
+    try {
+      if (!name.trim() || !email.trim()) {
+        return Alert.alert('Inscrição', 'Preencha todos os campos.');
+      }
+
+      setIsLoading(true);
+
+      const registerResponse = await api.post(`events/${EVENT_ID}/attendees`, {
+        name,
+        email,
+      });
+
+      const attendeeId = registerResponse.data.attendeeId;
+
+      if (attendeeId) {
+        const badgeResponse = await api.get(
+          `attendees/${attendeeId}/badge`
+        );
+ 
+        saveBadgeData(badgeResponse.data.badge);
+        Alert.alert('Inscrição', 'Inscrição realizada com sucesso!', [
+          { text: 'ver meu ticket', onPress: () => router.navigate('/ticket') },
+        ]);
+      }
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        console.log(err.response, 'Error');
+        if (
+          String(err.response?.data.message).includes(
+            'Attendee already registered.'
+          )
+        ) {
+          return Alert.alert('Inscrição', 'Esse e-mail já está cadastrado!');
+        }
+      }
+
+      setIsLoading(false);
+      Alert.alert('Inscrição', 'Não foi possível realizar a inscrição.');
     }
-
-    router.navigate('/ticket');
   }
 
   return (
     <View className="flex-1 items-center justify-center bg-green-500 p-8">
-      <StatusBar barStyle="light-content"/>
+      <StatusBar barStyle="light-content" />
       <Image
         source={require('@/assets/logo.png')}
         className="h-16 mb-12"
@@ -51,16 +92,20 @@ export default function Register() {
           <Input.Field
             placeholder="email"
             keyboardType="email-address"
-            textContentType='emailAddress'
-            autoComplete='email'
-            autoCapitalize='none'
+            textContentType="emailAddress"
+            autoComplete="email"
+            autoCapitalize="none"
             value={email}
             onChangeText={setEmail}
           />
         </Input>
       </View>
 
-      <Button title="Realizar inscrição" onPress={handleRegister} />
+      <Button
+        title="Realizar inscrição"
+        onPress={handleRegister}
+        isLoading={isLoading}
+      />
 
       <Link
         href="/"
